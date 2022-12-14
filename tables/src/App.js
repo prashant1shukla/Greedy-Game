8bedd18245c079b00b806869a98b59bd1ce01707
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2022-12-15 04:49:30 +0530

    Api-working and sorting, searching

--- tables/src/App.js
+++ tables/src/App.js
@@ -1,46 +1,18 @@
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import MaterialTable from 'material-table'
-import React, { useEffect, useState } from 'react';
+import MatDataTable from './Components/MatDataTable';
 
+class App extends Component {  
 
-function App() {
-  const [tableData, setData]=useState([])
-  const columns=[
-    // {title:"Name", field:"name"},
-    // {title:"Email", field:"email"},
-    // {title:"Phone Number", field:"phone"},
-    // {title:"Age", field:"age"},
-    // {title:"Gender", field:"gender"},
-    // {title:"City", field:"city"},
-    {title:"Date", field:"date"},
-    {title:"Apps", field:"apps"},
-    {title:"Clicks", field:"clicks"},
-    {title:"Requests", field:"requests"},
-    {title:"Revenue", field:"revenue"},
-    {title:"Fill Rate", field:"fill"},
-    {title:"CTR", field:"ctr"},
-  ]
-
-  useEffect(()=>{
-    fetch("http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03")
-    .then(resp=>resp.json())
-    .then(resp=>setData(resp))
-  },[])
-
-  //for api calls-data
-
-  return (
-    <div className="App">
-      <h2>Greedy Game</h2>
-
-      <MaterialTable 
-        title="Employee Data"
-        data={tableData}
-        columns={columns}
-      />
-    </div>
-  );
+  render() {
+    const spacing = 5;
+    return (      
+      <div className="App" > 
+        <h2>Greedy Game</h2>             
+         <MatDataTable />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ tables/src/Components/MatDataTable.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import axios from 'axios'; // npm instal axios
+import { forwardRef } from 'react';
+import MaterialTable from "material-table";
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Search from '@material-ui/icons/Search';
+import DetailTable from './DetailTable';
+
+const tableIcons = {
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+};
+
+export default class MatDataTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {person: []};   
+  }
+    
+  componentDidMount(prevProps) {    
+    // const maxResults = 20;    
+    // const url = `https://randomuser.me/api/?results=${maxResults}`;
+    const url = `http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03`;
+
+    axios.get(url)
+    .then(data => {
+      console.log(data);
+      console.log(data.data.data);
+      this.setState({ person: data.data.data });
+
+      var newArr = data.data.data.map(function(val) {          
+        return {
+        date: val.date,
+        app_id: val.app_id,
+        requests: val.requests,
+        responses: val.responses,          
+        impressions: val.impressions,          
+        clicks: val.clicks,          
+        revenue: val.revenue,
+        };
+      });
+      console.log(data.data.data); 
+      this.setState({
+        tableArray: newArr  //set state of the weather5days
+      },()=> {
+         console.log(this.state.tableArray); 
+         console.log('this.tableArray ',this.state.tableArray);
+      });      
+    });
+  }
+
+  render() {
+    return (      
+      <div style={{ maxWidth: "50%", marginLeft: "300px", marginTop: "100px" }}>
+        <MaterialTable
+          icons={tableIcons}
+          options={{
+            grouping: true
+          }}
+          detailPanel={rowData => {
+            return (              
+              <DetailTable />
+            )
+          }}
+          columns={[
+            {title:"Date", field:"date"},
+            {title:"Apps", field:"app_id"},
+            {title:"Requests", field:"requests"},
+            {title:"Response", field:"responses"},
+            {title:"Impressions", field:"impressions"},
+            {title:"Clicks", field:"clicks"},
+            {title:"Revenue", field:"revenue"},                               
+          ]}
+          data={this.state.tableArray}      
+        
+          title="Demo Title"
+        />
+      </div>
+    );
+  }
+}
+ 
