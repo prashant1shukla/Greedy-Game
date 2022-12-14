80a3288fc852d595150f937860b73776e30b80fe
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2022-12-14 20:48:01 +0530

    api fetch

--- tables/src/App.js
+++ tables/src/App.js
@@ -1,27 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import MaterialTable from 'material-table'
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
-  const [tableData, setTableData]=useState([])
+  const [tableData, setData]=useState([])
   const columns=[
-    {title:"Name", field:"name"},
-    {title:"Email", field:"email"},
-    {title:"Phone Number", field:"phone"},
-    {title:"Age", field:"age"},
-    {title:"Gender", field:"gender"},
-    {title:"City", field:"city"},
+    // {title:"Name", field:"name"},
+    // {title:"Email", field:"email"},
+    // {title:"Phone Number", field:"phone"},
+    // {title:"Age", field:"age"},
+    // {title:"Gender", field:"gender"},
+    // {title:"City", field:"city"},
+    {title:"Date", field:"date"},
+    {title:"Apps", field:"apps"},
+    {title:"Clicks", field:"clicks"},
+    {title:"Requests", field:"requests"},
+    {title:"Revenue", field:"revenue"},
+    {title:"Fill Rate", field:"fill"},
+    {title:"CTR", field:"ctr"},
   ]
+
+  useEffect(()=>{
+    fetch("http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03")
+    .then(resp=>resp.json())
+    .then(resp=>setData(resp))
+  },[])
 
   //for api calls-data
 
   return (
     <div className="App">
-      <h2>React-App</h2>
+      <h2>Greedy Game</h2>
 
-      <MaterialTable columns={columns} data={tableData} data={tableData}/>
+      <MaterialTable 
+        title="Employee Data"
+        data={tableData}
+        columns={columns}
+      />
     </div>
   );
 }
